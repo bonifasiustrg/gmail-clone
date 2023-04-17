f1a0070a3b813df7908b2f0c1c42c74b0c8c7e1c
Bonifasius Tarigan <bonifasius.tambak@gmail.com>
2023-04-17 12:10:28 +0700

    Navigationn Drawer Menu

---
+++ app/src/main/java/com/bonifasiustrg/gmailclone/DrawerMenuData.kt
@@ -0,0 +1,64 @@
+package com.bonifasiustrg.gmailclone
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.MailOutline
+import androidx.compose.material.icons.outlined.Email
+import androidx.compose.material.icons.outlined.MailOutline
+import androidx.compose.material.icons.outlined.Person
+import androidx.compose.material.icons.outlined.Send
+import androidx.compose.material.icons.outlined.Settings
+import androidx.compose.material.icons.outlined.Star
+import androidx.compose.ui.graphics.vector.ImageVector
+
+sealed class DrawerMenuData(
+    val icon: ImageVector? = null,
+    val title: String? = null,
+    val isDivider: Boolean = false,
+    val isHeader:Boolean = false
+){
+
+    object Mail: DrawerMenuData(
+        icon = Icons.Outlined.Email,
+        title = "Inbox"
+    )
+    object Person: DrawerMenuData(
+        icon = Icons.Outlined.Person,
+        title = "Contacts"
+    )
+    object Starred: DrawerMenuData(
+        icon = Icons.Outlined.Star,
+        title = "Starred"
+    )
+    object MailOutline: DrawerMenuData(
+        icon = Icons.Filled.MailOutline,
+        title = "All Mail"
+    )
+    object Send: DrawerMenuData(
+        icon = Icons.Outlined.Send,
+        title = "Send Mail"
+    )
+    object Setting: DrawerMenuData(
+        icon = Icons.Outlined.Settings,
+        title = "Settings"
+    )
+
+
+    object Divider: DrawerMenuData(
+        isDivider = true
+    )
+    object HeaderOne: DrawerMenuData(
+        isHeader = true,
+        title = "ALL LABELS"
+    )
+    object HeaderTwo: DrawerMenuData(
+        isHeader = true,
+        title = "GOOGLE APPS"
+    )
+}
+
+/*
+*  1. The image of actor in the title can be null because if it's a divider
+that we don't need to have an image and title
+* 2. And if it is not a divider and that is a header, then we need to have
+the title and not the icon.
+* */
--- app/src/main/java/com/bonifasiustrg/gmailclone/MainActivity.kt
+++ app/src/main/java/com/bonifasiustrg/gmailclone/MainActivity.kt
@@ -1,19 +1,33 @@
     package com.bonifasiustrg.gmailclone
 
 import android.os.Bundle
+import android.webkit.WebSettings.TextSize
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.DateRange
+import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material.icons.filled.Email
 import androidx.compose.material.icons.filled.Face
 import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Info
+import androidx.compose.material.icons.filled.MailOutline
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.filled.Send
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material.icons.filled.Star
+import androidx.compose.material3.Divider
 import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
@@ -34,6 +48,8 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.bonifasiustrg.gmailclone.component.GmailDrawerMenu
 import com.bonifasiustrg.gmailclone.component.HomeAppBar
 import com.bonifasiustrg.gmailclone.ui.theme.GmailCloneTheme
@@ -64,26 +80,50 @@ fun GmailApp() {
     val drawerState  = rememberDrawerState(initialValue = DrawerValue.Closed)
 
 //    val menuList = listOf(Icons.Filled.Menu to "Menu", Icons.Filled.Person to "Person")
-    val menuList = listOf(Icons.Default.Favorite to "Fovorite", Icons.Default.Face to "Face", Icons.Default.Email to "Email")
+    val menuList = listOf(
+        Icons.Filled.MailOutline to "All Inbox",
+        Icons.Filled.Email to "Inbox",
+        Icons.Filled.Email to "Unread",
+        Icons.Filled.Star to "Starred",
+        Icons.Filled.DateRange to "Snoozed",
+        Icons.Filled.Send to "Sent",
+        Icons.Filled.Send to "Scheduled",
+        Icons.Filled.Send to "Outbox",
+        Icons.Filled.Info to "Drafts",
+        Icons.Filled.MailOutline to "All Mail",
+        Icons.Filled.Info to "Spam",
+        Icons.Filled.Delete to "Trash",
+        Icons.Filled.DateRange to "Date",
+        Icons.Filled.Favorite to "Favorite",
+        Icons.Filled.Settings to "Settings",
+        Icons.Filled.Face to "Help"
+    )
+
     var selectedItem by remember { mutableStateOf(-1) }
 
     ModalNavigationDrawer(drawerState = drawerState,
         drawerContent = {
-            ModalDrawerSheet {
+            ModalDrawerSheet(modifier = Modifier.verticalScroll(rememberScrollState())
+            ) {
                 GmailDrawerMenu()
                 menuList.forEachIndexed(){idx, data->
                     NavigationDrawerItem(
-                        label = { 
-                                Row(modifier = Modifier.fillMaxWidth()){
-                                    Icon(
+                        label = {
+                            Row(modifier = Modifier
+                                    .fillMaxWidth()){
+                                Icon(
                                         imageVector = data.first,
                                         contentDescription = data.second,
                                     )
-                                    Text(text = data.second)
+                                Text(text = data.second, modifier = Modifier.padding(start = 8.dp))
+
                                 }
                         }, 
                         selected = selectedItem == idx,
-                        onClick = { selectedItem = idx }
+                        onClick = {
+                            selectedItem = idx
+                            // NAV CONTROLLER EHRE
+                        }
                     )
                 }
             }
